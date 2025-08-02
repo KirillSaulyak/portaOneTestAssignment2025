fa635f37f70fb7c7f293d28d7c90eeb593eef70b
Kirill Saulyak <kerich345@gmail.com>
2025-08-02 18:51:17 +0300

    assignment ready

---
+++ TestAssignment/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        RepairText repairText = new RepairText();
+        repairText.repair();
+    }
+}
